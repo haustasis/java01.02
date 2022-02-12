80b2b868ba14e89ae83b62d255e0c626bc903135
TARISO <toni.ariso@gmail.com>
2022-02-12 14:50:10 +0100

    clean ex4

--- nivell1/src/n1exercici4/App.java
+++ nivell1/src/n1exercici4/App.java
@@ -7,5 +7,26 @@ Exercici 4. Crea la teva pròpia classe d'excepcions, heretant de la classe Exce
  Captura i mostra el missatge emmagatzemat.
  Mostra també la classe de l'excepció.
  */
+
 public class App {
+
+    private static String password = "1234";
+
+    public static void main(String[] args) {
+
+        try {
+            setPassword("123456");
+        } catch (PasswordLengthException ex) {
+            System.err.println("[-] Missatge Error PassWord: " + ex.getMessage());
+            System.err.println("[-] Classe excepció: " + ex.getClass());
+        }
+    }
+
+    public static String setPassword(String newPassword) throws PasswordLengthException{
+        if(newPassword.length() > 5) {
+            throw new PasswordLengthException("Wrong password length");
+        }
+
+        return password = newPassword;
+    }
 }
---
+++ nivell1/src/n1exercici4/PasswordLengthException.java
@@ -0,0 +1,7 @@
+package n1exercici4;
+
+public class PasswordLengthException extends RuntimeException {
+    public PasswordLengthException(String msg) {
+        super(msg);
+    }
+}
