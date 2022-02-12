58d3927ee51ee35440d225d090ffe5d020b05791
TARISO <toni.ariso@gmail.com>
2022-02-12 12:19:38 +0100

    clean ex2

--- nivell1/src/n1exercici2/App.java
+++ nivell1/src/n1exercici2/App.java
@@ -1,10 +1,15 @@
 package n1exercici2;
-/*
-Exercici 2. Defineix una referència a un objecte i inicialitza-la amb null.
-Tracta d'invocar un mètode a través d'aquesta referència.
-Ara envolta el codi amb una clàusula try-catch per a capturar l'excepció.
-Mostra el missatge i el tipus d'excepció.
- */
 
 public class App {
+
+    public static void main(String[] args) {
+
+        try {
+            String str1 = null;
+            str1.length();
+        } catch (NullPointerException ex) {
+            System.err.println("[-] Missatge excepció Null: "+ ex.getMessage());
+        }
+    }
+
 }
